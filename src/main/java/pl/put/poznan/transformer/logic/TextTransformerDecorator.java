efdb336beb752304c342f5fa50d751e7e4292926
Rostislaw Chapeha <chapegarostislav@gmail.com>
2023-12-11 16:42:24 +0100

    Created TextTransformerDecorator

---
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformerDecorator.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.transformer.logic;
+
+public abstract class TextTransformerDecorator extends TextTransformer {
+    protected TextTransformer textToTransform;
+
+    public TextTransformerDecorator(TextTransformer textToTransform) {
+        this.textToTransform = textToTransform;
+    }
+
+    @Override
+    public abstract String transform();
+}
