66d47e39c478fd91d47d8fa83cc04b6e4863f4b7
RubyNaxela <rubynaxela@gmail.com>
2023-12-11 20:52:07 +0100

    Refactor number dictionaries in ExpNum

--- src/main/java/pl/put/poznan/transformer/logic/transform/ExpNum.java
+++ src/main/java/pl/put/poznan/transformer/logic/transform/ExpNum.java
@@ -3,71 +3,35 @@ package pl.put.poznan.transformer.logic.transform;
 import org.jetbrains.annotations.NotNull;
 import pl.put.poznan.transformer.logic.TextTransformer;
 import pl.put.poznan.transformer.logic.TextTransformerDecorator;
+import pl.put.poznan.transformer.util.DictionaryBuilder;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ExpNum extends TextTransformerDecorator {
 
-    private static final Map<Character, String> DIGIT_MAP = new HashMap<>();
-    private static final Map<String, String> TEENS_MAP = new HashMap<>();
-    private static final Map<Character, String> TENS_MAP = new HashMap<>();
-    private static final Map<Character, String> HUNDRED = new HashMap<>();
+    private static final Map<Integer, String> DIGITS
+            = new DictionaryBuilder<Integer, String>(10).with(0, "zero").with(1, "jeden").with(2, "dwa").with(3, "trzy")
+                                                        .with(4, "cztery").with(5, "pięć").with(6, "sześć").with(7, "siedem")
+                                                        .with(8, "osiem").with(9, "dziewięć").build();
+    private static final Map<Integer, String> TEENS
+            = new DictionaryBuilder<Integer, String>(0).with(10, "dziesięć").with(1, "jedenaście").with(2, "dwanaście")
+                                                       .with(3, "trzynaście").with(4, "czternaście").with(5, "piętnaście")
+                                                       .with(6, "szesnaście").with(7, "siedemnaście").with(8, "osiemnaście")
+                                                       .with(9, "dziewiętnaście").build();
+    private static final Map<Integer, String> TENS
+            = new DictionaryBuilder<Integer, String>(10).with(2, "dwadzieścia").with(3, "trzydzieści").with(4, "czterdzieści")
+                                                        .with(5, "pięćdziesiąt").with(6, "sześćdziesiąt").with(7, "siedemdziesiąt")
+                                                        .with(8, "osiemdziesiąt").with(9, "dziewięćdziesiąt").build();
+    private static final Map<Integer, String> HUNDREDS
+            = new DictionaryBuilder<Integer, String>(10).with(1, "sto").with(2, "dwieście").with(3, "trzysta")
+                                                        .with(4, "czterysta").with(5, "pięćset").with(6, "sześćset")
+                                                        .with(7, "siedemset").with(8, "osiemset").with(9, "dziewięćset")
+                                                        .build();
+    private final boolean numberExpandAllowed;
 
-    static {
-        DIGIT_MAP.put('0', "zero");
-        DIGIT_MAP.put('1', "jeden");
-        DIGIT_MAP.put('2', "dwa");
-        DIGIT_MAP.put('3', "trzy");
-        DIGIT_MAP.put('4', "cztery");
-        DIGIT_MAP.put('5', "pięć");
-        DIGIT_MAP.put('6', "sześć");
-        DIGIT_MAP.put('7', "siedem");
-        DIGIT_MAP.put('8', "osiem");
-        DIGIT_MAP.put('9', "dziewięć");
-    }
-
-    static {
-        TEENS_MAP.put("10", "dziesięć");
-        TEENS_MAP.put("11", "jedenaście");
-        TEENS_MAP.put("12", "dwanaście");
-        TEENS_MAP.put("13", "trzynaście");
-        TEENS_MAP.put("14", "czternaście");
-        TEENS_MAP.put("15", "piętnaście");
-        TEENS_MAP.put("16", "szesnaście");
-        TEENS_MAP.put("17", "siedemnaście");
-        TEENS_MAP.put("18", "osiemnaście");
-        TEENS_MAP.put("19", "dziewiętnaście");
-    }
-
-    static {
-        TENS_MAP.put('2', "dwadzieścia");
-        TENS_MAP.put('3', "trzydzieści");
-        TENS_MAP.put('4', "czterdzieści");
-        TENS_MAP.put('5', "pięćdziesiąt");
-        TENS_MAP.put('6', "sześćdziesiąt");
-        TENS_MAP.put('7', "siedemdziesiąt");
-        TENS_MAP.put('8', "osiemdziesiąt");
-        TENS_MAP.put('9', "dziewięćdziesiąt");
-    }
-
-    static {
-        HUNDRED.put('1', "sto");
-        HUNDRED.put('2', "dwieście");
-        HUNDRED.put('3', "trzysta");
-        HUNDRED.put('4', "czterysta");
-        HUNDRED.put('5', "pięćset");
-        HUNDRED.put('6', "sześćset");
-        HUNDRED.put('7', "siedemset");
-        HUNDRED.put('8', "osiemset");
-        HUNDRED.put('9', "dziewięćset");
-    }
-
-    private final boolean numberExpandAllow;
-
-    public ExpNum(@NotNull TextTransformer textToTransform, boolean numberExpandAllow) {
+    public ExpNum(@NotNull TextTransformer textToTransform, boolean numberExpandAllowed) {
         super(textToTransform);
-        this.numberExpandAllow = numberExpandAllow;
+        this.numberExpandAllowed = numberExpandAllowed;
     }
 
     @Override
@@ -76,12 +40,12 @@ public class ExpNum extends TextTransformerDecorator {
     }
 
     public @NotNull String function(@NotNull String s) {
-        if (numberExpandAllow) {
+        if (numberExpandAllowed) {
             final StringBuilder result = new StringBuilder();
             for (String part : s.split("\\D+")) {
                 try {
                     int numericValue = Integer.parseInt(part);
-                    result.append(expandNumber(numericValue)).append(" ");
+                    result.append(numberInWords(numericValue)).append(" ");
                 } catch (NumberFormatException e) {
                     result.append(part).append(" ");
                 }
@@ -93,7 +57,7 @@ public class ExpNum extends TextTransformerDecorator {
         }
     }
 
-    private @NotNull String expandNumber(int number) {
+    private @NotNull String numberInWords(int number) {
         if (number < 0 || number > 999) {
             return String.valueOf(number);
         }
@@ -102,28 +66,19 @@ public class ExpNum extends TextTransformerDecorator {
 
         int hundreds = number / 100;
         if (hundreds > 0) {
-            result.append(HUNDRED.get((char) (hundreds + '0')));
+            result.append(HUNDREDS.get(hundreds));
         }
 
         int remainder = number % 100;
         if (remainder > 0) {
-            if (result.length() > 0) {
-                result.append(" ");
-            }
-
-            if (remainder < 10) {
-                result.append(DIGIT_MAP.get((char) (remainder + '0')));
-            } else if (remainder < 20) {
-                result.append(TEENS_MAP.get(String.valueOf(remainder)));
-            } else {
-                int tens = remainder / 10;
-                int units = remainder % 10;
-
-                result.append(TENS_MAP.get((char) (tens + '0')));
-
-                if (units > 0) {
-                    result.append(" ").append(DIGIT_MAP.get((char) (units + '0')));
-                }
+            if (result.length() > 0) result.append(" ");
+            if (remainder < 10) result.append(DIGITS.get(remainder));
+            else if (remainder < 20) result.append(TEENS.get(remainder - 10));
+            else {
+                final int tens = remainder / 10;
+                final int units = remainder % 10;
+                result.append(TENS.get(tens));
+                if (units > 0) result.append(" ").append(DIGITS.get(units));
             }
         }
 
