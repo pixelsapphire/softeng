10afedc94cfe78b5cba34cf094b62546d58abb8e
RubyNaxela <rubynaxela@gmail.com>
2023-12-12 18:53:04 +0100

    Add JavaDoc to PluralVariant and PolishPluralVariant

--- src/main/java/pl/put/poznan/transformer/util/PluralVariant.java
+++ src/main/java/pl/put/poznan/transformer/util/PluralVariant.java
@@ -5,23 +5,70 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
 
+/**
+ * This interface represents a word plural conjugation variant. Specific implementations are used to conjugate words
+ * in different languages. The conjugation is based on the quantity of the word, which is passed as a parameter to the
+ * {@link #getVariant(long)} and {@link #getVariant(double)} methods. {@link #getQuantityName} methods are used to
+ * conjugate the word with a number name, e.g. "one apple" or "two apples", in which case a function that returns the
+ * number in words may be passed as a parameter.
+ */
 public interface PluralVariant {
 
+    /**
+     * Returns the plural variant of the word based on the inegral quantity.
+     *
+     * @param quantity the quantity of the word
+     * @return the plural variant of the word
+     */
     @NotNull String getVariant(long quantity);
 
+    /**
+     * Returns the plural variant of the word based on the fractional quantity.
+     *
+     * @param quantity the quantity of the word
+     * @return the plural variant of the word
+     */
     @NotNull String getVariant(double quantity);
 
+    /**
+     * Returns the quantity of the word with the number name, e.g. "1 apple" or "2 apples".
+     *
+     * @param quantity the quantity of the word
+     * @return the quantity of the word with the number name
+     */
     @NotNull
     default String getQuantityName(long quantity) {
         return getQuantityName(quantity, (Function<Long, String>) null);
     }
 
+    /**
+     * Returns the quantity of the word with the number name, e.g. "one apple" or "two apples". If the number name
+     * provider is null, the number itself is appended to the quantity name, e.g. "1 apple" or "2 apples".
+     *
+     * @param quantity           the quantity of the word
+     * @param numberNameProvider the function that returns the number in words
+     * @return the quantity of the word with the number name
+     */
     @NotNull String getQuantityName(long quantity, @Nullable Function<Long, String> numberNameProvider);
 
+    /**
+     * Returns the quantity of the word with the number name, e.g. "1 apple" or "2 apples".
+     *
+     * @param quantity the quantity of the word
+     * @return the quantity of the word with the number name
+     */
     @NotNull
     default String getQuantityName(double quantity) {
         return getQuantityName(quantity, null);
     }
 
+    /**
+     * Returns the quantity of the word with the number name, e.g. "one apple" or "two apples". If the number name
+     * provider is null, the number itself is appended to the quantity name, e.g. "1 apple" or "2 apples".
+     *
+     * @param quantity           the quantity of the word
+     * @param numberNameProvider the function that returns the number in words
+     * @return the quantity of the word with the number name
+     */
     @NotNull String getQuantityName(double quantity, @Nullable Function<Double, String> numberNameProvider);
 }
--- src/main/java/pl/put/poznan/transformer/util/PolishPluralVariant.java
+++ src/main/java/pl/put/poznan/transformer/util/PolishPluralVariant.java
@@ -5,10 +5,21 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
 
+/**
+ * An implementation of {@link PluralVariant} for Polish language.
+ */
 public class PolishPluralVariant implements PluralVariant {
 
     private final String single, plural2, plural1, fraction;
 
+    /**
+     * Creates a new instance of {@link PolishPluralVariant} with the given variants.
+     *
+     * @param single   the singular variant (for 1)
+     * @param plural1  the plural variant for numbers ending with 2, 3 or 4, except for numbers ending with 12, 13 or 14
+     * @param plural2  the plural variant for all other numbers
+     * @param fraction the plural variant for fractional numbers
+     */
     public PolishPluralVariant(@NotNull String single, @NotNull String plural1, @NotNull String plural2, @NotNull String fraction) {
         this.single = single;
         this.plural1 = plural1;
@@ -16,6 +27,7 @@ public class PolishPluralVariant implements PluralVariant {
         this.fraction = fraction;
     }
 
+    // see PluralVariant#getVariant for documentation
     @Override
     public @NotNull String getVariant(long quantity) {
         if (quantity == 1) return single;
@@ -23,18 +35,21 @@ public class PolishPluralVariant implements PluralVariant {
         else return plural2;
     }
 
+    // see PluralVariant#getVariant for documentation
     @Override
     public @NotNull String getVariant(double quantity) {
         if (quantity % 1 != 0) return fraction;
         else return getVariant((int) quantity);
     }
 
+    // see PluralVariant#getQuantityName for documentation
     @Override
     public @NotNull String getQuantityName(long quantity, @Nullable Function<Long, String> numberNameProvider) {
         if (numberNameProvider == null) return quantity + " " + getQuantityName(quantity);
         return (quantity != 1 ? numberNameProvider.apply(quantity) + " " : "") + getVariant(quantity);
     }
 
+    // see PluralVariant#getQuantityName for documentation
     @Override
     public @NotNull String getQuantityName(double quantity, @Nullable Function<Double, String> numberNameProvider) {
         if (numberNameProvider == null) return quantity + " " + getQuantityName(quantity);
