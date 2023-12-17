2a50b92551a43c8f8c97462bd42a2a0e28f08c13
RubyNaxela <rubynaxela@gmail.com>
2023-12-18 00:43:10 +0100

    Change the server port to 1203

--- src/main/java/pl/put/poznan/transformer/server/app/TextTransformerServer.java
+++ src/main/java/pl/put/poznan/transformer/server/app/TextTransformerServer.java
@@ -6,6 +6,8 @@ import pl.put.poznan.transformer.server.logic.TransformerBase;
 import pl.put.poznan.transformer.server.logic.TransformsRegister;
 import pl.put.poznan.transformer.server.logic.transform.*;
 
+import java.util.Collections;
+
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.server.rest"})
 public class TextTransformerServer {
 
@@ -18,6 +20,8 @@ public class TextTransformerServer {
         TransformsRegister.register(new ReverseTransform(new TransformerBase(""), false));
         TransformsRegister.register(new ShortcutTransform(new TransformerBase(""), ShortcutTransform.Type.IDENTITY));
 
-        SpringApplication.run(TextTransformerServer.class, args);
+        final SpringApplication server = new SpringApplication(TextTransformerServer.class);
+        server.setDefaultProperties(Collections.singletonMap("server.port", "1203"));
+        server.run(args);
     }
 }
